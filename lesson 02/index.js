56ff5e12116f7563a17d59ad42104f43dbe2dfed
IrinaKUCHINA <99545917+IrinaKUCHINA@users.noreply.github.com>
2022-02-15 11:11:07 +0500

    Add files via upload

---
+++ lesson 02/index.js
@@ -0,0 +1,22 @@
+'use strict';
+{
+    let productNameOne = 'oranges';
+    let oranges = 23;
+    let number = 18;
+    const categoryOranges = 'фрукты';
+    let countPriceOranges = oranges*number;
+    
+    console.log(categoryOranges);
+    console.log(countPriceOranges);
+}
+
+{
+    let productNameTwo = 'toy';
+    let toy = 18;
+    let count = 22;
+    const categoryToy = 'игрушки';
+    let countPriceToy = toy*number;
+    
+    console.log(categoryToy);
+    console.log(countPriceToy);
+}
