863dfedf22657f3078efbf4da68478edf241b45c
IrinaKUCHINA <99545917+IrinaKUCHINA@users.noreply.github.com>
2022-02-25 08:22:48 +0500

    Add files via upload

---
+++ lesson 06/task02.js
@@ -0,0 +1,16 @@
+'use strict';
+
+function isPrime (num) {  
+    
+    for (let i = 2; i < num; i++) {
+        if (num%i === 0 ) {
+        return false;
+        } 
+
+        else {
+        return true;
+        }
+    }
+}
+
+console.log(isPrime(7));
