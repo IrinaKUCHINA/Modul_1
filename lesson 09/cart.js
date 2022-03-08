5fbd17751a53e9774722c71bafa7ec7f50eaf59e
IrinaKUCHINA <99545917+IrinaKUCHINA@users.noreply.github.com>
2022-03-08 17:18:10 +0500

    Update cart.js

--- lesson 09/cart.js
+++ lesson 09/cart.js
@@ -11,9 +11,9 @@ const cart = {
 
   add(productName, priceProduct, amountProduct) {
     cart.items = {
-      productName: ['apples', 'cakes', 'books'],
-      priceProduct: [200, 500, 400],
-      amountProduct: [2, 5, 3],
+      productName: ['apples', 200, 2],
+      priceProduct: ['cakes', 500, 5],
+      amountProduct: ['apples', 400, 3],
     };
   },
 
@@ -21,28 +21,44 @@ const cart = {
     cart.count += n;
   },
 
+// У меня вопросы, сама разобраться не могу, вроде данные передаются в cart.item, потому что через консоль они выходят
+// но тогда поломались все остальные формулы, length не работает потому что это объект?
+
+// add(productName, priceProduct, amountProduct) {
+//   cart.items = {
+//     productName,
+//     priceProduct,
+//     amountProduct],
+//   };
+// },  я делала так, тогда я могу передать данные в items, но только один раз
+//  при введении новых данных они перезаписываются на старые
+//  можно мне объяснить как это работает?, по clear нашла только удаление
+
   calculateItemPrice() {
     let sum = 0;
 
     for (let i = 0; i < cart.items.length; i++) {
-      sum += cart.add().priceProduct[i] * cart.add().amountProduct[i];
+      sum += cart.items.priceProduct[i] * cart.items.amountProduct[i];
       cart.totalPrice = sum;
     }
-    return sum;
+    return console.log(cart.totalPrice);
   },
 
   clear() {
-    cart = [];
+    cart.items = '';
   },
 
   print() {
     console.log(JSON.stringify(cart.items));
     console.log(cart.totalPrice);
+    console.log(cart.clear());
   },
 };
-cart.calculateItemPrice();
+cart.calculateItemPrice('toy');
 cart.print();
-cart.add();
+cart.add('toy', 150, 2);
+
 
 console.log(cart.increaseCount(3));
 console.log(cart.items);
+
