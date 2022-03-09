35d764680f9b28ee8a0743c7bc051b76ac0aea5c
IrinaKUCHINA <99545917+IrinaKUCHINA@users.noreply.github.com>
2022-03-09 16:16:56 +0500

    Update cart.js

--- lesson 09/cart.js
+++ lesson 09/cart.js
@@ -10,55 +10,41 @@ const cart = {
   },
 
   add(productName, priceProduct, amountProduct) {
-    cart.items = {
-      productName: ['apples', 200, 2],
-      priceProduct: ['cakes', 500, 5],
-      amountProduct: ['apples', 400, 3],
+    const item = {
+      name: productName,
+      price: priceProduct,
+      count: amountProduct,
     };
+    cart.items.push(item);
+    cart.calculateItemPrice();
+    cart.increaseCount(amountProduct);
   },
 
   increaseCount(n) {
     cart.count += n;
   },
 
-// У меня вопросы, сама разобраться не могу, вроде данные передаются в cart.item, потому что через консоль они выходят
-// но тогда поломались все остальные формулы, length не работает потому что это объект?
-
-// add(productName, priceProduct, amountProduct) {
-//   cart.items = {
-//     productName,
-//     priceProduct,
-//     amountProduct],
-//   };
-// },  я делала так, тогда я могу передать данные в items, но только один раз
-//  при введении новых данных они перезаписываются на старые
-//  можно мне объяснить как это работает?, по clear нашла только удаление
-
   calculateItemPrice() {
     let sum = 0;
-
     for (let i = 0; i < cart.items.length; i++) {
-      sum += cart.items.priceProduct[i] * cart.items.amountProduct[i];
-      cart.totalPrice = sum;
+      sum += cart.items.price[i] * (cart.items.count[i] + cart.count);
+      cart.items.totalPrice = sum;
     }
-    return console.log(cart.totalPrice);
   },
 
   clear() {
-    cart.items = '';
+    cart.items = [];
+    cart.totalPrice = 0;
+    cart.count = 0;
   },
 
   print() {
     console.log(JSON.stringify(cart.items));
     console.log(cart.totalPrice);
-    console.log(cart.clear());
   },
 };
-cart.calculateItemPrice('toy');
+
+cart.add('toy', 150, 2),
+cart.add('apples', 250, 4);
 cart.print();
-cart.add('toy', 150, 2);
-
-
-console.log(cart.increaseCount(3));
-console.log(cart.items);
-
+cart.clear();
